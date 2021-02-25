7e59d1daf124fdcd8c4d395de9cd947f02f6d114
Abangpa1ace <dyek72731@gmail.com>
2021-02-26 03:19:23 +0900

    디렉토리 구조수정

--- src/Store.ts
+++
@@ -1,8 +0,0 @@
-import { create } from 'domain';
-import { applyMiddleware, compose, createStore } from 'redux';
-import RootReducer from './Reducers/RootReducer'
-
-const Store = createStore(
-  RootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
